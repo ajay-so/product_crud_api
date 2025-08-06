8c0e6ffb33b617d9f9b0053ca974c0f2c8811e2a
Ajay Kumar <ajaykumar89693479@gmail.com>
2025-08-06 19:33:31 +0530

    check the helmet and express-rate-limit package

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const productRoutes = require("./routes/productRoutes");
 const authRoutes = require("./routes/authRoutes");
+// const rateLimit = require("express-rate-limit");
+// const cron = require("node-cron");
+
+// Cron Job
+// cron.schedule("* * * * *", () => {
+//   console.log("Cron job executed");
+// });
+
+// const limiter = rateLimit({
+// 	windowMs: 1 * 60 * 1000, // 1 minutes
+// 	limit: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+// 	message : "Too many requests from this IP, please try again after 15 minutes"
+// })
+
+// Apply the rate limiting middleware to all requests.
+
 
 const app = express();
 const port = process.env.PORT || 3002;
@@ -12,6 +28,8 @@ const port = process.env.PORT || 3002;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// app.use(limiter)
 
 // Routes
 app.use("/products", productRoutes);
